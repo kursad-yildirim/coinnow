6e4af80a7370b8f3cd82548738f8b29032596ade
kursad-yildirim <kursad,yildirim@outlook.com>
2021-02-17 14:39:15 +0300

    Registry name added to comatiner path

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+REGISTRY='tripko'
 CONTAINER='/usr/bin/podman'
 WORKDIR='/home/workspace'
 APP="coinnow"
 TAG=$1
 PORT='52380'
 
-$CONTAINER build $WORKDIR/$APP/code.dev -t $APP:$TAG
+$CONTAINER build $WORKDIR/$APP/code.dev -t $REGISTRY/$APP:$TAG
