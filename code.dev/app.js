57831fb95dc5e96341f006019feb5d6389a9b11c
kursad-yildirim <kursad,yildirim@outlook.com>
2021-02-18 22:18:54 +0300

    GCM_Binance_[Get Symbol List and Price]_a-0.5.6-debug

--- code.dev/app.js
+++ code.dev/app.js
@@ -4,44 +4,30 @@ var markets = {
 	binance: {
 		url: 'https://api.binance.com',
 		apiKeyName: 'binance-api-key',
-		apiSecretName: 'binance-api-secret'
+		apiSecretName: 'binance-api-secret',
+		testUrlExtension: '/api/v3/ping',
+		symbolListUrlExtension: '/api/v3/exchangeInfo'
 	}
 }
 
-//testConnection('binance');
 getSymbolList('binance');
 //getSymbolPrice('binance', 'BNBUSDT');
 
 // FUNCTIONS
-function testConnection(marketName) {
-	console.log('Testing ' + marketName + ' network  connection:');
-	var acConTest = {
-		method: 'get',
-		url: markets[marketName].url + '/api/v3/ping'
-	};
-	axios(acConTest)
-		.then(function (response) {
-			console.log('-> Connection Test Succeeded!');
-		})
-		.catch(function (error) {
-			console.log('-> Connection Test Failed!');
-		});
-
-}
 
 function getSymbolList(marketName) {
 	console.log('Getting symbol list from ' + marketName + ' network:');
 	var acGetSymbolList = {
 		method: 'get',
-		url: markets[marketName].url + '/api/v3/exchangeInfo'
+		url: markets[marketName].url + markets[marketName].symbolListUrlExtension
 	};
-	console.log(acGetSymbolList);
 	axios(acGetSymbolList)
 		.then(function (response) {
-			for (var symbolIndex = 0; symbolIndex < response.data.symbols.length(); symbolIndex++){
-				console.log(response.data.symbols[symbolIndex].symbol);
+//			for (var symbolIndex = 0; symbolIndex < response.data.symbols.length(); symbolIndex++){
+//				console.log(response.data.symbols[symbolIndex].symbol);
 //				getSymbolPrice(marketName, response.data.symbols[symbolIndex].symbol);
-			}
+//			}
+console.log(response.data.symbols.length());
 		})
 		.catch(function (error) {
 			console.log('--> failed to get symbol list');
@@ -60,4 +46,23 @@ function getSymbolPrice(marketName, symbolName) {
 		.catch(function (error) {
 			console.log(error);
 		});
+}
+
+
+
+
+function testConnection(marketName) {
+	console.log('Testing ' + marketName + ' network  connection:');
+	var acConTest = {
+		method: 'get',
+		url: markets[marketName].url + markets[marketName].testUrlExtension
+	};
+	axios(acConTest)
+		.then(function (response) {
+			console.log('-> Connection Test Succeeded!');
+		})
+		.catch(function (error) {
+			console.log('-> Connection Test Failed!');
+		});
+
 }
