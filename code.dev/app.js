519b5d3ee34f49b6c43cf73cd1dfe65c67b826ff
kursad-yildirim <kursad,yildirim@outlook.com>
2021-02-19 15:53:27 +0300

    GCM_MultipleMarkets_[Get Symbol List and Price]_a-0.9.7-d

--- code.dev/app.js
+++ code.dev/app.js
@@ -40,11 +40,8 @@ var symbolShortList = ["BTC", "ETH", "XTZ", "LTC", "ADA", "XLM"];
 var myOrders = {};
 // Get Prices for my ShortList
 for (var mySymbolIndex = 0; mySymbolIndex < symbolShortList.length; mySymbolIndex++) {
-	getSymbolPrice('binance', getpairName(symbolShortList[mySymbolIndex], 'binance'));
-}/*
-for (var mySymbolIndex = 0; mySymbolIndex < symbolShortList.length; mySymbolIndex++) {
-	getSymbolPrice('btcturk', getpairName(symbolShortList[mySymbolIndex], 'btcturk'));
-}*/
+	getSymbolPrice('btcturk', getpairName(symbolShortList[mySymbolIndex], 'binance'));
+}
 
 //getSymbolList('binance');
 
