dceb2cfb0ba2383ab4f1c6592ea183dadcb9005a
kursad-yildirim <kursad,yildirim@outlook.com>
2021-02-19 09:50:35 +0300

    GCM_BtcTurk_[Get Symbol List and Price]_a-0.7.4-d

--- code.dev/app.js
+++ code.dev/app.js
@@ -13,14 +13,14 @@ var markets = {
 		headerParams: [
 			{name: 'X-PCK', value: '012e4da8-3e38-47d0-99ba-18f2f523f3e6'},
 			{name: 'X-Stamp', value: Math.floor(new Date().getTime() / 1000)},
-			{name: 'X-Signature', value: crypto.createHmac('SHA256', "gAgM7shFpPrgHVf2rvi80rrTZr0b3SR6").update('012e4da8-3e38-47d0-99ba-18f2f523f3e6' + Math.floor(new Date().getTime() / 1000)).digest('base64');}
+			{name: 'X-Signature', value: crypto.createHmac('SHA256', "gAgM7shFpPrgHVf2rvi80rrTZr0b3SR6").update('012e4da8-3e38-47d0-99ba-18f2f523f3e6' + Math.floor(new Date().getTime() / 1000)).digest('base64')}
 		],
 		testUrlExtension: 'none',
 		symbolListUrlExtension: '/api/v2/ticker',
 		symbolPriceUrlExtension: '/api/v2/ticker?pairSymbol=BTC_USDT'
 	}
 }
-
+console.log(markets.btcturk);
 var symbolShortList = ["BTCUSDT", "ETHUSDT", "XTZUSDT", "LTCUSDT", "ADAUSDT", "XLMUSDT"];
 
 var myOrders = {};
