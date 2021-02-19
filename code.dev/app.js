42204c980adf60297525750aaefee4b41aba100f
kursad-yildirim <kursad,yildirim@outlook.com>
2021-02-19 09:42:16 +0300

    GCM_BtcTurk_[Get Symbol List and Price]_a-0.7

--- code.dev/app.js
+++ code.dev/app.js
@@ -1,23 +1,39 @@
 const axios = require('axios');
+const crypto = require('crypto');
 
 var markets = {
 	binance: {
 		url: 'https://api.binance.com',
-		apiKeyName: 'binance-api-key',
-		apiSecretName: 'binance-api-secret',
 		testUrlExtension: '/api/v3/ping',
 		symbolListUrlExtension: '/api/v3/exchangeInfo',
 		symbolPriceUrlExtension: '/api/v3/ticker/price?symbol='
+	},
+	btcturk: {
+		url: 'https://api.btcturk.com',
+		headerParams: [
+			{name: 'X-PCK', value: '012e4da8-3e38-47d0-99ba-18f2f523f3e6'},
+			{name: 'X-Stamp', value: Math.floor(new Date().getTime() / 1000)},
+			{name: 'X-Signature', value: crypto.createHmac('SHA256', "gAgM7shFpPrgHVf2rvi80rrTZr0b3SR6").update('012e4da8-3e38-47d0-99ba-18f2f523f3e6' + Math.floor(new Date().getTime() / 1000)).digest('base64');}
+		],
+		testUrlExtension: 'none',
+		symbolListUrlExtension: '/api/v2/ticker',
+		symbolPriceUrlExtension: '/api/v2/ticker?pairSymbol=BTC_USDT'
 	}
 }
 
 var symbolShortList = ["BTCUSDT", "ETHUSDT", "XTZUSDT", "LTCUSDT", "ADAUSDT", "XLMUSDT"];
 
-// Get Prices for my ShortList
+var myOrders = {
+
+}
+/*
+// Get Prices for my ShortList: Binance
 console.log('market: binance');
 for (var mySymbolIndex = 0; mySymbolIndex < symbolShortList.length; mySymbolIndex++){
 	getSymbolPrice('binance', symbolShortList[mySymbolIndex]);
-}
+}*/
+
+getSymbolList('btcturk');
 
 
 // FUNCTIONS
