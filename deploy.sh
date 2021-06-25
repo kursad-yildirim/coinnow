2b9ca6dab2bd14e17049bd1ea72f9e3aec5e31fa
kursad-yildirim <kursad,yildirim@outlook.com>
2021-06-26 01:49:15 +0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,71 @@
 #!/bin/bash
 REGISTRY='registry.8-mega.io'
 CONTAINER='/usr/bin/docker'
-
 WORKDIR='/home/workspace'
-APP="8-mega-kurus"
+APP="home-crypto"
 MICROSERVICE="getsymbolprice"
 TAG=$1
 PORT='52380'
+APPDIR=$WORKDIR/$APP/$MICROSERVICE
+NAMESPACE="8-mega-apps"
 
-$CONTAINER build $WORKDIR/$APP/$MICROSERVICE/code.dev -t $REGISTRY/$MICROSERVICE-$APP:$TAG
-$CONTAINER push $REGISTRY/$MICROSERVICE-$APP:$TAG
+# Create Dockerfile
+cat > $APPDIR/code.dev/Dockerfile << EOLDOCKERFILE
+FROM node:lts-alpine3.13
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm install
+COPY . .
+EXPOSE $PORT
+CMD [ "npm", "start" ]
+EOLDOCKERFILE
+# Build and push docker image
+sudo $CONTAINER build $WORKDIR/$APP/$MICROSERVICE/code.dev -t $REGISTRY/$MICROSERVICE-$APP:$TAG
+sudo $CONTAINER push $REGISTRY/$MICROSERVICE-$APP:$TAG
 
+# Create k8s resource  yaml files
+cat > $APPDIR/kube.resource.files/$MICROSERVICE-pod.yaml << EOLPODYAML
+apiVersion: v1
+kind: Pod
+metadata:
+  name: $MICROSERVICE
+  namespace: $NAMESPACE
+  labels:
+    app: $APP
+    microservice: $MICROSERVICE
+spec:
+  containers:
+    - name: $MICROSERVICE-container
+      image: $REGISTRY/$MICROSERVICE-$APP:$TAG
+      ports:
+        - name: $MICROSERVICE-port
+          protocol: TCP
+          containerPort: $PORT
+EOLPODYAML
+cat > $APPDIR/kube.resource.files/$MICROSERVICE-svc.yaml << EOLSVCYAML
+apiVersion: v1
+kind: Service
+metadata:
+  name: $MICROSERVICE
+  namespace: $NAMESPACE
+  labels:
+    app: $APP
+    microservice: $MICROSERVICE
+spec:
+  selector:
+    app: $APP
+    microservice: $MICROSERVICE
+  type: ClusterIP
+  ports:
+    - name: $MICROSERVICE-port
+      protocol: TCP
+      port: 52380
+      targetPort: 52380
+EOLSVCYAML
 
-#apiVersion: v1
-#kind: Pod
-#metadata:
-#  name: get-symbol-price
-#  namespace: 8-mega-apps
-#  labels:
-#    app: 8-mega-kurus
-#    microservice: getsymbolprice
-#spec:
-#  containers:
-#    - name: 8mgk-gsp
-#      image: registry.8-mega.io/getsymbolprice-8-mega-kurus:0.3
-#      ports:
-#        - name: 8m-nodejs-port
-#          protocol: TCP
-#          containerPort: 52380
-#kubectl -n 8-mega-apps edit deployment set --image=registry.8-mega.io/getsymbolprice-8-mega-kurus:0.3
-#kubectl create -f kube.resource.files/pod.yaml
-#kubectl -n 8-mega-apps delete pod get-symbol-price
+# delete existing  kube resources
+kubectl -n $NAMESPACE delete pod $MICROSERVICE
+kubectl -n $NAMESPACE delete svc $MICROSERVICE
+# create new kube resources
+kubectl create -f $APPDIR/kube.resource.files/$MICROSERVICE-pod.yaml
+kubectl create -f $APPDIR/kube.resource.files/$MICROSERVICE-svc.yaml
